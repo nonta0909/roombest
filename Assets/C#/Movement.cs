738f0bff299a24f921ed5ae8c48e0905ed592f6a
野田雄太 <ny11994869@gmail.com>
2020-11-13 17:19:25 +0900

    プロジェクト作成

---
+++ Assets/C#/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  public void Button0(){
+    this.transform.localPosition = new Vector2(0,2000);
+  }
+  public void Button1(){
+    this.transform.localPosition = new Vector2(0,4000);
+  }
+  public void Button2(){
+    this.transform.localPosition = new Vector2(0,6000);
+  }
+}
---
+++ Assets/C#/Quiz.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour
+{
+  public void Button(){
+    this.transform.localPosition = new Vector2(-2000,0);
+  }
+  }
